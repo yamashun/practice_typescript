29d18940a4be6eeae2167107e2f62b48894bb159
yamashun <yshunske@gmail.com>
2019-10-23 22:59:01 +0900

    Javascriptも含む場合

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,3 @@
-export function test1() {
-  return 'test1'
-}
-export function test2() {
-  return { value: 'test2'}
-}
+import { sampleText, sampleFunction } from './sample'
+const a = sampleFunction()
+const b = sampleText

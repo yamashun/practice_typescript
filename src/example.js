59f423a255eda719a0357aa9daaff1ae58ec8d12
yamashun <yshunske@gmail.com>
2022-03-06 16:04:35 +0900

    2-2-2 意図しないNAN

--- src/example.js
+++
@@ -1,5 +0,0 @@
-function expo2(amount) {
-  return amount ** 2
-}
-const value = expo2('1,000')
-const withTax = value * 1.1
---
+++ src/example.ts
@@ -0,0 +1,5 @@
+function expo2(amount: number) {
+  return amount ** 2
+}
+const value = expo2(1000)
+const withTax = value * 1.1
